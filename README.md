c2e5750332925bd2cbe9611469ec752fad62dddc
Han Li <Han.Li@capitalone.com>
2018-02-04 23:35:05 -0500

    init

---
+++ README.md
@@ -0,0 +1,2 @@
+static site generator for cccgermantown.org
+
---
+++ gatsby-config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  siteMetadata: {
+    siteName: `CCCG Site`,
+  },
+  plugins: [
+    // Add typescript stack into webpack
+    `gatsby-plugin-typescript`,
+  ],
+}
