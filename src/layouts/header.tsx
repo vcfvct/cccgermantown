d679199edef84a28e7d9edff26dbc0defd75b4b6
Han Li <vcfvct@gmail.com>
2018-02-16 22:29:11 -0500

    add some true church style

--- src/layouts/header.tsx
+++ src/layouts/header.tsx
@@ -3,6 +3,7 @@ import Link from 'gatsby-link'
 import { Container, Navbar, NavbarBrand, NavbarToggler, Collapse, Nav, NavItem, UncontrolledDropdown, DropdownToggle, DropdownMenu, DropdownItem } from "reactstrap";
 import { NavLink } from "react-router-dom";
 
+import './header.css'
 interface State {
   isOpen: boolean;
 }
@@ -25,7 +26,7 @@ export default class MainNav extends React.Component<{}, State> {
     return (
       <Container>
 
-        <Navbar color="faded" light expand="md">
+        {/* <Navbar color="faded" light expand="md">
           <NavbarBrand href="/">
             <img src="https://s3.amazonaws.com/cccg-media/site-images/cccg-logo-72x385.png" />
           </NavbarBrand>
@@ -54,7 +55,27 @@ export default class MainNav extends React.Component<{}, State> {
               </UncontrolledDropdown>
             </Nav>
           </Collapse>
-        </Navbar>
+        </Navbar> */}
+        <header className="site-header">
+          <div className="container">
+            <a href="#" className="branding">
+              <img src="images/logo-cccg.png" alt="" className="logo" />
+            </a>
+
+            <div className="main-navigation">
+              <button className="menu-toggle"><i className="fa fa-bars"></i> Menu</button>
+              <ul className="menu">
+                <li className="menu-item current-menu-item"> <Link to="/" className="nav-link">主页 <small>Homepage</small></Link></li>
+                <li className="menu-item"><Link to="/about" className="nav-link">教会简介<small>About Us</small></Link></li>
+                <li className="menu-item"><a href="seremons.html">活动与通讯 <small>Activities</small></a></li>
+                <li className="menu-item"><a href="events.html">教会事工 <small>Sunt in culpa</small></a></li>
+                <li className="menu-item"><a href="families.html">多媒体文档 <small>Multimedia</small></a></li>
+                <li className="menu-item"><a href="#">联系我们 <small>Contact US</small></a></li>
+              </ul>
+            </div>
+            <div className="mobile-navigation"></div>
+          </div>
+        </header>
       </Container>
 
       // <div
