38187a14420ad19da53f881a593d6ea41c0c99b2
Han Li <vcfvct@gmail.com>
2021-07-05 11:02:01 -0400

    update packages and ts strict

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,9 +1,9 @@
-const path = require('path')
+const path = require('path');
 
-exports.createPages = ({graphql, boundActionCreators}) => {
-  const {createPage} = boundActionCreators
+exports.createPages = ({ graphql, actions }) => {
+  const { createPage } = actions;
   return new Promise((resolve, reject) => {
-    const pageTemplate = path.resolve('src/templates/page.tsx')
+    const pageTemplate = path.resolve('src/templates/page.tsx');
     resolve(
       graphql(`
         {
@@ -17,19 +17,19 @@ exports.createPages = ({graphql, boundActionCreators}) => {
         }
       `).then((result) => {
         if (result.errors) {
-          reject(result.errors)
+          reject(result.errors);
         }
         result.data.allContentfulPage.edges.forEach((edge) => {
-          createPage ({
+          createPage({
             path: edge.node.slug,
             component: pageTemplate,
             context: {
               slug: edge.node.slug
             }
-          })
-        })
-        return
+          });
+        });
+        return;
       })
-    )
-  })
-}
+    );
+  });
+};
